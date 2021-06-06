9ae791b4be7bd6b5c01dade00b77bd3ac7e697b6
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-06-06 16:37:15 +0900

    ✨ Getting startedについて学習 実際にnormalのtoastを実装

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import toast, { Toaster } from 'react-hot-toast'
+
+const notify = () => toast('Here is your toast.')
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <button onClick={notify}>Make me a toast</button>
+      <Component {...pageProps} />
+      <Toaster />
+    </div>
+  )
 }
 
 export default MyApp
