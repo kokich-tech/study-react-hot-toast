edbf299745a9c1fd52ba2fe79e20c0f5e6955c1c
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-06-06 17:29:52 +0900

    ⚡️ loadingについて学ぶ

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,15 @@
 import '../styles/globals.css'
 import toast, { Toaster } from 'react-hot-toast'
 
-const notify = () =>
-  toast('Hello World', {
-    //duration 表示秒数　1000=1秒
-    duration: 4000,
-    // Styling スタイリングを変更することができる
-    style: { backgroundColor: 'orange' },
-    className: '',
-    // Custom Icon
-    icon: '👏',
-    // Change colors of success/error/loading icon
-    iconTheme: {
-      primary: '#000',
-      secondary: '#fff',
-    },
-    // Aria　アクセシビリティに考慮
-    ariaProps: {
-      role: 'status',
-      'aria-live': 'polite',
-    },
-  })
-
 function MyApp({ Component, pageProps }) {
+  const handleClick = () => toast.loading('Waiting...')
+
   return (
     <div>
-      <button onClick={notify}>Make me a toast</button>
+      <button onClick={handleClick}>Make me a toast</button>
       <Component {...pageProps} />
       <Toaster />
     </div>
   )
 }
-
 export default MyApp
