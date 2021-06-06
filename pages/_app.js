a76b8c2368b924354524c1c7fd62c8f96e2681b0
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-06-06 17:02:04 +0900

    ⚡️ toast()APIについて学習

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
 import toast, { Toaster } from 'react-hot-toast'
 
-const notify = () => toast('Here is your toast.')
+const notify = () =>
+  toast('Hello World', {
+    //duration 表示秒数　1000=1秒
+    duration: 4000,
+    // Styling スタイリングを変更することができる
+    style: { backgroundColor: 'orange' },
+    className: '',
+    // Custom Icon
+    icon: '👏',
+    // Change colors of success/error/loading icon
+    iconTheme: {
+      primary: '#000',
+      secondary: '#fff',
+    },
+    // Aria　アクセシビリティに考慮
+    ariaProps: {
+      role: 'status',
+      'aria-live': 'polite',
+    },
+  })
 
 function MyApp({ Component, pageProps }) {
   return (
