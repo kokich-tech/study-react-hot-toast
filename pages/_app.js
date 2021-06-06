f6f38099f1e3686aa6d4c6ab287f6bdd34f8daf1
kokich-tech <68147142+kokich-tech@users.noreply.github.com>
2021-06-06 18:39:31 +0900

    ⚡️ <Toaster />について学ぶ

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,33 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Component {...pageProps} />
-      <Toaster />
+      <Toaster
+        // toasterの表示位置
+        position="top-center"
+        reverseOrder={false}
+        //余白について
+        gutter={8}
+        containerClassName=""
+        //toastがどこから出てくるかを指定できる
+        containerStyle={{}}
+        toastOptions={{
+          // Define default options
+          className: '',
+          duration: 5000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+          // Default options for specific types
+          success: {
+            duration: 3000,
+            theme: {
+              primary: 'green',
+              secondary: 'black',
+            },
+          },
+        }}
+      />
     </div>
   )
 }
